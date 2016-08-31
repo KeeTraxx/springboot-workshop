c36309538336382e009cef3aab185d440e5e3732
Khôi Tran <kt@compile.ch>
2016-08-31 21:33:23 +0200

    Initial commit

---
+++ src/main/java/ch/puzzle/springboot/workshop/Application.java
@@ -0,0 +1,14 @@
+package ch.puzzle.springboot.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ktran on 8/30/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
